b5401ac7bc01e6cbee003ae11ba55847de528f74
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-07-17 16:13:05 +0200

    fix: integrate fontawesome

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+// Importa il router
 import { router } from './router';
 
 // Importa FontAwesome
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faArrowDown, faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { faArrowDown, faAngleDown, faChartPie, faUsers, faChartSimple } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
+// Importa il CSS di FontAwesome
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false; // Disabilita l'inclusione automatica del CSS
+
 // Aggiungi le icone che vuoi usare nella libreria
-library.add(faArrowDown, faAngleDown);
+library.add(faArrowDown, faAngleDown, faChartPie, faUsers, faChartSimple);
 
 const app = createApp(App);
 
