8eecdcbdbce9a53e76674612dbf06fabddabd12b
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-07-17 12:50:37 +0200

    feat: text and part of second section

---
+++ src/page/HomaPageSection/SectionCard.vue
@@ -0,0 +1,60 @@
+<script>
+export default {
+  name: "SectionCard",
+};
+</script>
+
+<template>
+  <div class="sfondo">
+    <div class="container">
+      <h2>
+        Excellence in
+        <span class="evidenziata"
+          >Services <span class="sottolineatura"></span
+        ></span>
+      </h2>
+      <div class="contFlex">
+        <p>
+          We are leaders in providing consultancy services with a set of
+          cutting-edge technologies and a team of experienced and renowned
+          professionals. These are some otions that you can hire.
+        </p>
+        <div class="button">SEE ALL</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../../style/partials/variable.scss" as *;
+@use "../../style/partials/mixin.scss" as *;
+@use "../../style/general.scss" as *;
+
+.sfondo {
+  padding: 5rem 0;
+  width: 100%;
+  background-color: #e2e3e7;
+
+  h2 {
+    font-size: 3rem;
+    .evidenziata {
+      position: relative;
+      color: #058283;
+      .sottolineatura {
+        height: 1rem;
+        width: 4rem;
+        background-color: #c0dade;
+      }
+    }
+  }
+  .contFlex {
+    display: flex;
+    gap: 2rem;
+    justify-content: space-between;
+    p {
+      flex: 0 0 80%;
+      font-size: 1.5rem;
+    }
+  }
+}
+</style>
--- src/page/HomePage.vue
+++ src/page/HomePage.vue
@@ -1,45 +1,22 @@
 <script >
 import JamboPart from "./HomaPageSection/JamboPart.vue";
+import SectionCard from "./HomaPageSection/SectionCard.vue";
 export default {
   name: "HomePage",
   components: {
     JamboPart,
+    SectionCard,
   },
 };
 </script>
 
 <template>
   <JamboPart />
-  <!-- <div class="sfondoAnimato">
-    <div class="container">
-      <div class="jambo">
-        <div class="contImg">
-          <img src="/public/bg-parallax.png" alt="" />
-        </div>
-        <div class="contText">
-          <h2></h2>
-        </div>
-      </div>
-    </div>
-  </div> -->
+  <SectionCard />
 </template>
 
 <style lang="scss" scoped>
 @use "../style/partials/variable.scss" as *;
 @use "../style/partials/mixin.scss" as *;
 @use "../style/general.scss" as *;
-// .sfondoAnimato {
-//   background-color: #f1f1f1;
-//   .container {
-//     .jambo {
-//       height: 100vh;
-//       .contImg {
-//         height: 100%;
-//         img {
-//           height: 100%;
-//         }
-//       }
-//     }
-//   }
-// }
 </style>
