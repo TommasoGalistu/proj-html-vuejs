358163b08a265a8b85aabbb7b73e38780321a20f
DecimTheJudge <paolo1piga@gmail.com>
2024-07-17 15:12:36 +0200

    shop ed inizio carte

---
+++ src/page/shopSection/ArticlesCards.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+    name: "ArticlesCards",
+}; 
+</script>
+
+<template>
+
+    <div class="ShopCards">
+        <div class="upper-img">
+            <div class="sale"></div>
+        </div>
+        <div class="lower-price">
+            <h2></h2>
+            <span></span>
+            <i></i>
+
+        </div>
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+    @use "src/style/general.scss" as *;
+    .ShopCards{
+        height: 150px;
+        width: 100px;
+    }
+
+    .upper-img{
+        height: 70%;
+        width: 100%;
+    }
+
+    .lower-price{
+        height: 30%;
+        width: 100%;
+    }
+    
+    .sale{
+        background-color: aqua;
+
+    }
+</style>
