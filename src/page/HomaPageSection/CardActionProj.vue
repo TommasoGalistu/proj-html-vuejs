576dc679f14451193efca5ee2e24cda3af1daeab
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-07-17 18:33:40 +0200

    fix:card style

--- src/page/HomaPageSection/CardActionProj.vue
+++ src/page/HomaPageSection/CardActionProj.vue
@@ -6,7 +6,27 @@ export default {
 
 <template>
   <div class="card">
-    <img src="" alt="" />
+    <div class="contText">
+      <div class="upPart">
+        <div>
+          <font-awesome-icon :icon="['far', 'user']" />
+          <span>Coding</span>
+        </div>
+        <div>
+          <font-awesome-icon :icon="['far', 'clock']" />
+          <span>3 Years</span>
+        </div>
+      </div>
+      <div class="bottomPart">
+        <h2>Portfolio e sti cazzi non so scrivo dopo</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate..
+        </p>
+      </div>
+    </div>
+    <div class="contImg">
+      <img src="/public/avocado.jpg" alt="avocado" />
+    </div>
   </div>
 </template>
 
@@ -16,8 +36,46 @@ export default {
 @use "../../style/general.scss" as *;
 
 .card {
+  position: relative;
   height: 300px;
   width: 300px;
   background-color: grey;
+  border-radius: 10px;
+  color: rgb(180, 180, 180);
+  .contText {
+    position: absolute;
+
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    background-color: rgba($color: black, $alpha: 0.5);
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding: 2rem 0;
+    .upPart {
+      display: flex;
+      gap: 1rem;
+      justify-content: center;
+      align-items: center;
+    }
+    .bottomPart {
+      h2 {
+        padding: 1rem 0;
+      }
+    }
+  }
+  .contImg {
+    border-radius: 10px;
+    height: 100%;
+    width: 100%;
+    img {
+      border-radius: 10px;
+      height: 100%;
+      width: 100%;
+    }
+  }
 }
 </style>
--- src/page/shopSection/ArticlesCards.vue
+++ src/page/shopSection/ArticlesCards.vue
@@ -1,44 +1,40 @@
 <script>
 export default {
-    name: "ArticlesCards",
-}; 
+  name: "ArticlesCards",
+};
 </script>
 
 <template>
-
-    <div class="ShopCards">
-        <div class="upper-img">
-            <div class="sale"></div>
-        </div>
-        <div class="lower-price">
-            <h2></h2>
-            <span></span>
-            <i></i>
-
-        </div>
+  <div class="ShopCards">
+    <div class="upper-img">
+      <div class="sale"></div>
     </div>
-
+    <div class="lower-price">
+      <h2></h2>
+      <span></span>
+      <i></i>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-    @use "src/style/general.scss" as *;
-    .ShopCards{
-        height: 150px;
-        width: 100px;
-    }
+@use "src/style/general.scss" as *;
+.ShopCards {
+  height: 150px;
+  width: 100px;
+}
 
-    .upper-img{
-        height: 70%;
-        width: 100%;
-    }
+.upper-img {
+  height: 70%;
+  width: 100%;
+}
 
-    .lower-price{
-        height: 30%;
-        width: 100%;
-    }
-    
-    .sale{
-        background-color: aqua;
+.lower-price {
+  height: 30%;
+  width: 100%;
+}
 
-    }
+.sale {
+  background-color: aqua;
+}
 </style>
