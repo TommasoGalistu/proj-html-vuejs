05b788104c1a9384d03539ecb8fa324a97d8dc36
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-07-17 14:54:40 +0200

    fix: photo fixed in second section

--- src/page/HomaPageSection/SecondSection.vue
+++ src/page/HomaPageSection/SecondSection.vue
@@ -6,7 +6,20 @@ export default {
 
 <template>
   <div class="sfondo">
-    <div class="container">CIAO SEMPLICEMENTE CIAO</div>
+    <div class="container">
+      <div class="contText">
+        <h2>The Company</h2>
+        <p>
+          FOr 12 years we have been providing audit and warranty, financial
+          advice, risk advice, taxes and related services to select client
+        </p>
+        <div class="contCard"></div>
+      </div>
+      <div class="contImg">
+        <div class="sfuocatura"></div>
+        <div class="fixed-image"></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,6 +33,39 @@ export default {
   background-color: #0d1b2a;
   color: grey;
   .container {
+    display: flex;
+    .contImg {
+      width: 50%;
+      position: relative;
+      overflow-y: scroll;
+      height: 80vh;
+      .sfuocatura {
+        position: absolute;
+        z-index: 1;
+        background: rgba(
+          255,
+          255,
+          255,
+          0.8
+        ); /* Sfondo semitrasparente per il contenuto */
+        padding: 20px;
+        box-sizing: border-box;
+      }
+      .fixed-image {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-image: url("/public/about-4.jpg");
+        background-size: contain;
+        background-attachment: fixed;
+        background-position: right -4%;
+        background-repeat: no-repeat;
+
+        // z-index: -1;
+      }
+    }
   }
 }
 </style>
