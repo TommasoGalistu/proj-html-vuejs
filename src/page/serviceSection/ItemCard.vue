fb1f59e24f7253c2948a277a3ac7f4cbd08d7c9d
Oleksandra Ubska <alessandraubska@gmail.com>
2024-07-18 18:19:01 +0200

    fix: icon

--- src/page/serviceSection/ItemCard.vue
+++ src/page/serviceSection/ItemCard.vue
@@ -22,7 +22,9 @@ export default {
 
       <!-- card core business -->
       <div class="service-card">
-        <div class="icon"><i class="fa-solid fa-chart-pie"></i></div>
+        <div class="icon">
+          <font-awesome-icon icon="fa-solid fa-chart-pie" />
+        </div>
         <h2>Core Business</h2>
         <p>
           It takes innovative approaches to transform, modernize, and run
@@ -32,7 +34,9 @@ export default {
 
       <!-- card performance -->
       <div class="service-card">
-        <div class="icon"><i class="fa-solid fa-gauge-high"></i></div>
+        <div class="icon">
+          <font-awesome-icon icon="fa-solid fa-gauge-high" />
+        </div>
         <h2>Performance</h2>
         <p>
           Achieving maximum impact and value from investments in finance and
