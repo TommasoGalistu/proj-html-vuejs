d3ee708f610939076d6658be5421501c9df2e897
DecimTheJudge <paolo1piga@gmail.com>
2024-07-18 12:17:48 +0200

    added images, fabag icon, shop card static update

--- src/page/shopSection/ArticlesCards.vue
+++ src/page/shopSection/ArticlesCards.vue
@@ -6,25 +6,45 @@ export default {
 
 <template>
 
-    <div class="ShopCards">
+    <a href="#"><div class="ShopCards">
         <div class="upper-img">
-            <div class="sale"></div>
+            <div class="sale">
+                <div class="sale1"></div>
+                <div class="sale2"></div>
+            </div>
+            <img src="/public/clothes/black_hoodie.jpg" alt="">
         </div>
         <div class="lower-price">
-            <h2></h2>
-            <span></span>
-            <i></i>
+            <h2>Black Hooded sweater</h2>
+            <div class="scritte-container">
+                <span>200$ $129</span>
+                <a href="#"><font-awesome-icon icon="bag-shopping" /></a>
+            </div>
 
         </div>
-    </div>
+    </div></a>
 
 </template>
 
 <style lang="scss" scoped>
     @use "src/style/general.scss" as *;
+
+    a{
+        text-decoration: none;
+    }
+    .fa-bag-shopping{
+        font-size: 25px;
+        color: rgb(255, 255, 255);
+        
+        background-color: rgb(75, 159, 155);
+        padding: 8px;
+        border-radius: 50%;
+    }
     .ShopCards{
-        height: 150px;
-        width: 100px;
+        height: 380px;
+        width: 280px;
+        background-color: yellow;
+        border-radius: 10px;
     }
 
     .upper-img{
@@ -32,13 +52,53 @@ export default {
         width: 100%;
     }
 
+    .lower-price h2{
+        padding-top: 20px;
+    }
+    .upper-img img{
+        height: 100%;
+        width: 100%;
+        border-radius: 10px;
+    }
+
     .lower-price{
         height: 30%;
         width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    .scritte-container{
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+
+    }
+
+    .scritte-container span{
+        font-size: 22px;
     }
     
     .sale{
         background-color: aqua;
+        width: 60px;
+        height: 30px;
+        position: absolute;
+        border-radius: 20%;
+        margin-top: 30px;
+        display: flex;
+        flex-direction: row;
+    }
 
+    .sale1{
+        width: 90%;
+        height: 100%;
+    }
+    .sale2{
+        
     }
 </style>
--- src/store.js
+++ src/store.js
@@ -2,92 +2,159 @@ import { reactive } from 'vue'
 
 export const store = reactive({
     // card nella terza sezione di home chiamate da metodo
-    cardHomePage:[
-        
+    cardHomePage: [
+
         {
-            ricerca: ['events','social','technology'],
-            path:'/public/cuffie.avif',
+            ricerca: ['events', 'social', 'technology'],
+            path: '/public/cuffie.avif',
             tipo: 'Coding',
             tempo: '3 years',
             titolo: 'Portfolio item with intro with sidebar',
             text: 'lorem ipsum bla bla bla sit consectetur adisipsiscing elit. Vestibulu...',
-            
+
         },
         {
-            ricerca: ['enviroment','innovation'],
-            path:'/public/cuffie2.avif',
+            ricerca: ['enviroment', 'innovation'],
+            path: '/public/cuffie2.avif',
             tipo: 'Coding',
             tempo: '3 years',
             titolo: 'Portfolio item with intro with sidebar',
             text: 'lorem ipsum bla bla bla sit consectetur adisipsiscing elit. Vestibulu...',
-            
+
         },
         {
-            ricerca: ['social','technology', 'innovation'],
-            path:'/public/fiore.avif',
+            ricerca: ['social', 'technology', 'innovation'],
+            path: '/public/fiore.avif',
             tipo: 'Coding',
             tempo: '3 years',
             titolo: 'Portfolio item with intro with sidebar',
             text: 'lorem ipsum bla bla bla sit consectetur adisipsiscing elit. Vestibulu...',
-            
+
         },
         {
-            ricerca:['istitutional','technology'],
-            path:'/public/ciliegie.jpg',
+            ricerca: ['istitutional', 'technology'],
+            path: '/public/ciliegie.jpg',
             tipo: 'Coding',
             tempo: '3 years',
             titolo: 'Portfolio item with intro with sidebar',
             text: 'lorem ipsum bla bla bla sit consectetur adisipsiscing elit. Vestibulu...',
-            
+
         },
         {
             ricerca: ['enviroment', 'events', 'istitutional'],
-            path:'/public/vaso.avif',
+            path: '/public/vaso.avif',
             tipo: 'Coding',
             tempo: '3 years',
             titolo: 'Portfolio item with intro with sidebar',
             text: 'lorem ipsum bla bla bla sit consectetur adisipsiscing elit. Vestibulu...',
-            
+
         },
         {
-            ricerca: ['enviroment','innovation'],
-            path:'/public/avocado.jpg',
+            ricerca: ['enviroment', 'innovation'],
+            path: '/public/avocado.jpg',
             tipo: 'Coding',
             tempo: '3 years',
             titolo: 'Portfolio item with intro with sidebar',
             text: 'lorem ipsum bla bla bla sit consectetur adisipsiscing elit. Vestibulu...',
-            
+
         },
     ],
     menu: [
         {
-            titolo:'all',
-        active:true
-        }, 
-        {
-            titolo:'enviroment',
-        active:false
-        }, 
-        {
-            titolo:'events',
-        active:false
+            titolo: 'all',
+            active: true
         },
         {
-            titolo:'istitutional',
-        active:false
+            titolo: 'enviroment',
+            active: false
         },
         {
-            titolo:'social',
-        active:false
+            titolo: 'events',
+            active: false
         },
         {
-            titolo:'technology',
-        active:false
-        }, 
+            titolo: 'istitutional',
+            active: false
+        },
         {
-            titolo:'innovation',
-        active:false
+            titolo: 'social',
+            active: false
+        },
+        {
+            titolo: 'technology',
+            active: false
+        },
+        {
+            titolo: 'innovation',
+            active: false
         }
     ],
+    clothes: [
+        {
+            nome: "Black hooded sweatshirt",
+            prezzoOriginale: "$200.00",
+            prezzoAttuale: "$129.00",
+            img: "/public/clothes/black_hoodie.jpg",
+        },
+        {
+            nome: "Black t-shirt with lamp print",
+            prezzoAttuale: "$45.00",
+            img: "/public/clothes/black_tshirt.jpg",
+        },
+        {
+            nome: "Blue hooded sweatshirt",
+            prezzoOriginale: "$129.50",
+            prezzoAttuale: "$89.90",
+            img: "/public/clothes/blue_hoodie.jpg",
+        },
+        {
+            nome: "Brown hooded sweatshirt",
+            prezzoOriginale: "$75.00",
+            prezzoAttuale: "$39.50",
+            img: "/public/clothes/brown_hoodie.jpg",
+        },
+        {
+            nome: "Gray hooded sweatshirt",
 
+            prezzoAttuale: "$149.00",
+            img: "/public/clothes/gray_hoodie.jpg",
+        },
+        {
+            nome: "Navy Blue hooded sweatshirt",
+
+            prezzoAttuale: "$79.00",
+            img: "/public/clothes/navy_blue_hoodie.jpg",
+        },
+        {
+            nome: "Ninja square poster",
+            prezzoOriginale: "$60.00",
+            prezzoAttuale: "$35.00",
+            img: "/public/clothes/ninja_square_poster.jpg",
+        },
+        {
+            nome: "Red hooded sweatshirt",
+
+            prezzoAttuale: "$45.90",
+            img: "/public/clothes/red_hoodie.jpg",
+        },
+        {
+            nome: "Skull square poster",
+
+            prezzoAttuale: "$20.00",
+            img: "/public/clothes/skull_poster.jpg",
+        },
+        {
+            nome: "White ninja print t-shirt",
+
+            prezzoAttuale: "$90.00",
+            img: "/public/clothes/white_ninja_print.jpg",
+        }
+    ]
 });
+
+
+
+
+
+
+
