e5e1374ef10f5ec392accbeab79be724740a388b
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-07-19 19:40:59 +0200

    fix: modify shop page v-for

--- src/page/ShopPage.vue
+++ src/page/ShopPage.vue
@@ -2,46 +2,44 @@
 import ArticlesCards from "./shopSection/ArticlesCards.vue";
 import ArticlesSorting from "./shopSection/ArticlesSorting.vue";
 import Slider from "./shopSection/Slider.vue";
-import {store} from "../store";
-
-
-
+import { store } from "../store";
 
 export default {
   name: "ShopPage",
-  methods: {
-        
-  },
-  components :{
+  methods: {},
+  components: {
     ArticlesCards,
     ArticlesSorting,
     Slider,
   },
   data() {
     return {
-        store,
-        vestiti : store.clothes,
+      store,
+      vestiti: store.clothes,
     };
-}};
-
-
+  },
+};
 </script>
 
 <template>
-  
   <div class="container">
-    
     <section class="articles">
       <div class="results">
-        
-        <span><font-awesome-icon icon="fa-solid fa-bars" /> Showing N-N of N results </span>
-        <ArticlesSorting v-for="(vestito,index) in vestiti" :key="index" :elemento="vestito" />
+        <span
+          ><font-awesome-icon icon="fa-solid fa-bars" /> Showing N-N of N
+          results
+        </span>
+        <ArticlesSorting />
       </div>
-      <ArticlesCards/>
+      <ArticlesCards
+        v-for="(vestito, index) in vestiti"
+        :key="index"
+        :elemento="vestito"
+      />
     </section>
     <section class="prices">
       <h3>Filter by price</h3>
-      <Slider/>
+      <Slider />
     </section>
   </div>
 </template>
@@ -51,28 +49,26 @@ export default {
 @use "../style/partials/mixin.scss" as *;
 @use "../style/general.scss" as *;
 
-
-.container{
+.container {
   background-color: white;
   height: 300vh;
   display: flex;
   flex-direction: row;
-  
 }
 
-section{
+section {
   height: 90%;
   margin-top: 200px;
 }
 
-.articles{
+.articles {
   width: 70%;
   background-color: red;
   display: flex;
   flex-direction: column;
 }
 
-.results{
+.results {
   width: 100%;
   height: 3%;
   display: flex;
@@ -80,16 +76,16 @@ section{
   justify-content: space-between;
 }
 
-.results span{
+.results span {
   font-size: 20px;
 }
 
-.prices{
+.prices {
   width: 30%;
   background-color: green;
 }
 
-.sliderContainer{
+.sliderContainer {
   width: 80%;
 }
 .range-slider {
@@ -100,51 +96,51 @@ section{
   height: 6em;
 }
 
-.range-slider input[type=range] {
+.range-slider input[type="range"] {
   position: absolute;
   left: 0;
   bottom: 0;
 }
 
-input[type=number] {
+input[type="number"] {
   border: 1px solid #ddd;
   text-align: center;
   font-size: 1.6em;
   -moz-appearance: textfield;
 }
 
-input[type=number]::-webkit-outer-spin-button,
-input[type=number]::-webkit-inner-spin-button {
+input[type="number"]::-webkit-outer-spin-button,
+input[type="number"]::-webkit-inner-spin-button {
   -webkit-appearance: none;
 }
 
-input[type=number]:invalid,
-input[type=number]:out-of-range {
+input[type="number"]:invalid,
+input[type="number"]:out-of-range {
   border: 2px solid #ff6347;
 }
 
-input[type=range] {
+input[type="range"] {
   -webkit-appearance: none;
   width: 100%;
 }
 
-input[type=range]:focus {
+input[type="range"]:focus {
   outline: none;
 }
 
-input[type=range]:focus::-webkit-slider-runnable-track {
+input[type="range"]:focus::-webkit-slider-runnable-track {
   background: #2497e3;
 }
 
-input[type=range]:focus::-ms-fill-lower {
+input[type="range"]:focus::-ms-fill-lower {
   background: #2497e3;
 }
 
-input[type=range]:focus::-ms-fill-upper {
+input[type="range"]:focus::-ms-fill-upper {
   background: #2497e3;
 }
 
-input[type=range]::-webkit-slider-runnable-track {
+input[type="range"]::-webkit-slider-runnable-track {
   width: 100%;
   height: 5px;
   cursor: pointer;
@@ -155,11 +151,11 @@ input[type=range]::-webkit-slider-runnable-track {
   border: 0;
 }
 
-input[type=range]::-webkit-slider-thumb {
+input[type="range"]::-webkit-slider-thumb {
   z-index: 2;
   position: relative;
   box-shadow: 0px 0px 0px #000;
   border: 1px solid #2497e3;
   height: 18px;
-} 
+}
 </style>
